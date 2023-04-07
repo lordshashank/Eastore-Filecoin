27ae1b901837a85865e8bd7b97093fb7aa77c51e
lordforever <shashanktrivedi1917@gmail.com>
2023-04-08 03:57:07 +0530

    app.js

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,17 @@ const app = express();
 const axios = require("axios");
 const multer = require("multer");
 const path = require("path");
+const runNode = require("./api/run-node-function");
 const upload = require("./api/upload");
 const checkPrice = require("./api/checkPrice");
 const minerInfo = require("./api/miner-info");
 const buyDeal = require("./api/buyDeal");
 const retrieveDeal = require("./api/retrieveDeal");
+const buyPerpetualDeal = require("./api/buyPerpetualDeal");
 const push = require("./api/push");
+const answer = require("./api/chatgpt");
+const saveInDatabase = require("./api/saveInDatabase");
+const getFromDatabase = require("./api/getFromDatabase");
 // const sendData = require("./api/bu");
 app.get("/", (req, res) => {
   res.json("Hello, World!");
@@ -35,12 +40,18 @@ app.use((req, res, next) => {
 
   next();
 });
+app.use(runNode);
 app.use(upload);
 app.use(checkPrice);
 app.use(minerInfo);
 app.use(buyDeal);
 app.use(retrieveDeal);
+// app.use(sendData);
+app.use(buyPerpetualDeal);
 app.use(push);
+app.use(answer);
+app.use(saveInDatabase);
+app.use(getFromDatabase);
 
 app.listen(process.env.PORT || 3001, () => {
   console.log("Server is listening on port 3001");
